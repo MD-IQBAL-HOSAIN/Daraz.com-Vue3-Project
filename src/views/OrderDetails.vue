86b424fe1c92bd85dfb0d081d7a617d3a1e6b436
MD IQBAL HOSSAIN <152992612+MD-IQBAL-HOSAIN@users.noreply.github.com>
2024-07-14 21:35:00 +0600

    maked invoice

--- src/views/OrderDetails.vue
+++ src/views/OrderDetails.vue
@@ -1,28 +1,60 @@
 <template>
-    <div class="order-details">
-        <h2 class="text-center">Order Details</h2> <hr>
-        <div>
-        <router-link :to="{ name: 'OrderHistory' }" class="btn btn-primary">Back</router-link>
-    </div>
-        <div v-if="order" class="card mb-3">
-            <div class="card-header">
-                Order placed on {{ order.date }}
+    <h2 class="text-center">Invoice</h2>
+    <hr>
+    <div class="d-flex justify-content-center">
+        <div v-if="order" class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col-md-4 order-md-first">
+                        <h6 class="card-title">Invoice #{{ order.id }}</h6>
+                        <span class="card-text">Date: {{ order.date }}</span><br>
+                        <span class="card-text">Name: {{ order.name }}</span><br>
+                        <span class="card-text">Phone: {{ order.phone }}</span><br>
+                    </div>
+                    <div class="col-md-8 order-md-last">
+                        <img src="/assets/logo.png" alt="" height="60" width="200" style="border-radius: 30px;"><br>
+                        <h3>Daraz.com</h3>
+                        <small>shop address: Mirpur-10</small><br>
+                        <small>shop contact: 0123456789</small><br>
+                        <small>shop email: daraz@gmail.com</small>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- <h5 class="card-title">Items</h5> -->
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Items</th>
+                                    <th>Quantity</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(item, index) in order.items" :key="index">
+                                    <td>{{ item.title }}</td>
+                                    <td>{{ item.quantity }}</td>
+                                    <td>${{ item.price }}</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2"><b>Total:</b></td>
+                                    <td><b>${{ order.total }}</b></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item" v-for="(item, index) in order.items" :key="index">
-                    Name: {{ item.title }} <br>
-                    Quantity: {{ item.quantity }} <br>
-                    Rate: $ {{ item.price }} <br> 
-                    <!-- Total: ${{ (item.price * item.quantity).toFixed(2) }} -->
-                </li>
-                <li class="list-group-item">Total: ${{ order.total }}</li>
-            </ul>
         </div>
         <div v-else>
-            <p class="text-center">Order not found</p>
+            <p class="text-center">Invoice not found</p>
         </div>
     </div>
-
+    <div class="row d-flex justify-content-center mt-2">
+        <router-link :to="{ name: 'OrderHistory' }" class="col-md-2 btn btn-outline-primary">Back</router-link>
+        <button class="col-md-2 btn btn-outline-success" @click="printOrder">Print</button>
+    </div>
 </template>
 
 <script>
@@ -30,6 +62,11 @@ import { mapState } from 'vuex'
 export default {
     name: 'OrderDetails',
     params: ['id'],
+    methods: {
+        printOrder() {
+            window.print();
+        },
+    },
     computed: {
         ...mapState(['orders']),
         order() {
--- src/views/OrderHistory.vue
+++ src/views/OrderHistory.vue
@@ -7,24 +7,27 @@
     <table class="table table-striped">
       <thead>
         <tr>
+          <th>#</th>
           <th>Order ID</th>
           <th>Date</th>
           <th>Customer Name</th>
           <th>Phone</th>
+          <th>Address</th>
           <th>Total</th>
           <th>Actions</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="order in orders" :key="order.date">
+          <td>{{ orders.indexOf(order) + 1 }}</td>
           <td>{{ order.id }}</td>
           <td>{{ order.date }}</td>
           <td>{{ order.name }}</td>
           <td>{{ order.phone }}</td>
+          <td>{{ order.address }}</td>
           <td>$ {{ order.total }}</td>
           <td>
-            <router-link :to="{ name: 'OrderDetails', params: { id: order.id } }" class="btn btn-primary">View
-              Details</router-link>
+            <router-link :to="{ name: 'OrderDetails', params: { id: order.id } }" class="btn btn-primary">Invioce</router-link>
           </td>
         </tr>
       </tbody>
